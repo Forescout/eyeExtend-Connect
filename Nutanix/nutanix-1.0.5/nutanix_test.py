515ccce39109005667b520b12b1f397f38b39aad
Rao Peraka <rao.peraka@forescout.com>
2025-11-14 21:42:25 +0000

    OF-877: Adding files of nutanix-1.0.5

---
+++ Nutanix/nutanix-1.0.5/nutanix_test.py
@@ -0,0 +1,52 @@
+from base64 import b64encode
+import requests
+import logging
+import json
+
+logging.info('===>Starting Nutanix Test Script')
+
+prism_api_ip = params.get('connect_nutanix_prism_api_ip', '')
+username = params.get('connect_nutanix_username', '')
+password = params.get('connect_nutanix_password', '')
+
+# prepare encoded credentials based on username/password
+encoded_credentials = b64encode(bytes(f'{username}:{password}',\
+                                      encoding='ascii')).decode('ascii')
+# prepare the headers
+auth_header = f'Basic {encoded_credentials}'
+
+headers = { 
+            'Accept': 'application/json', 
+            'Content-Type': 'application/json',
+            'Authorization': f'{auth_header}', 
+            'cache-control': 'no-cache'
+          }
+
+# base api URI
+base_uri = f'https://{prism_api_ip}:9440/api/nutanix/v3'
+url = f'{base_uri}/hosts/list'
+data = '{"kind":"host"}'
+response = {}
+
+try: 
+    resp = requests.request('post', url, data=data, headers=headers, verify=False)
+
+    # Return the 'response' dictionary, must have a 'succeded' field.
+    
+    if resp.status_code == 200: 
+        content = json.loads(resp.content)
+        total_hosts = content.get('metadata', {'total_matches':0}).get('total_matches')
+        
+        response['succeeded'] = True
+        response['result_msg'] = f'Successfully connected. Total of {total_hosts} host(s) has been discovered.'
+        logging.debug(f'Test Script was successful. Total of {total_hosts} host(s) has been discovered.')
+    else:
+        response['succeeded'] = False
+        response['result_msg'] = 'Could not connect to Nutanix Server'
+        logging.info(f'Test Script was not successful - status code: {resp.status_code}')
+
+except requests.exceptions.RequestException as e: 
+    response['succeeded'] = False
+    response['result_msg'] = f'Could not connect to Nutanix Server.. Exception occured: {e}'
+
+logging.info('===>Ending Nutanix Test Script')
